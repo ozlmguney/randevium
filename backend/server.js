75b4a3c0300b5f6d990fbac4dd244e285c548a76
Özlem Güney <guney.ozlm@gmail.com>
2026-03-09 14:51:16 +0300

    old version

--- backend/server.js
+++ backend/server.js
@@ -60,9 +60,6 @@ app.get('/api/doctors', (req, res) => {
     res.json(doctors);
 });
 
-app.get('/api/test', (req, res) => {
-    res.json({ message: "Backend'e ulaşıldı, her şey yolunda!" });
-});
 
 app.post('/api/login', (req, res) => {
     const { email, password } = req.body;
